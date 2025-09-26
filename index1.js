fdaaa8cce01f02d5ad9c61a3489b78963787930c
Tedd Kim <teddkimnskills@gmail.com>
2025-09-26 11:52:49 +0300

    changed with redirect

--- index1.js
+++ index1.js
@@ -1,27 +1,33 @@
 const express = require('express');// calls the server to serve😅
 const app = express();
 
+
 const PORT = 3000;
 
-// Home route (optional), just friendly message to guide user to 'path'
+
+// Home route/message/redirect (optional)
 app.get('/', (req, res) => {
-  res.send('🎲 Dice Roller! Use /roll?faces=6&count=1 to roll dice.');
-});//use res.redirect to land to specific 'service'
+  res.redirect('/roll');
+});
+
 
 // Universal dice roller: d6, d20, dANY
 app.get('/roll', (req, res) => {
-  // Read query parameters
+  //reads query parameters
   const faces = parseInt(req.query.faces) || 6;  // default = 6-sided
   const count = parseInt(req.query.count) || 1;  // default = 1 die
+
 
   // Create an array of rolls
   const rolls = Array.from({ length: count }, () =>
     Math.floor(Math.random() * faces) + 1
   );
 
+
   // Calculate total sum, of result in current array
   const total = rolls.reduce((a, b) => a + b, 0);
 
+ 
   // Send pretty JSON response, cleaner
   res.json({
     faces: faces, 
@@ -30,6 +36,7 @@ app.get('/roll', (req, res) => {
     total: total
   }, null, 2);
 });
+
 
 // Start server or tap into
 app.listen(PORT, () => console.log(`🎲 Dice API at http://localhost:${PORT}`));
